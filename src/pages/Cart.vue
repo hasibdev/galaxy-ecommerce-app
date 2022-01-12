998042fd1a2ccb6a8dc2a14dc265c7ecff65a384
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-01-12 20:26:03 +0600

    check upated

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -34,36 +34,36 @@
       <section v-if="true">
          <!-- Check box -->
          <div class="flex justify-between">
-            <q-checkbox v-model="check1" label="Select All" />
-            <span v-if="check1" class="text-primary">Delete All</span>
+            <q-checkbox @click="onCheckAll" v-model="checkAll" label="Select All" />
+            <span v-if="selected.length" class="text-primary">Delete All</span>
          </div>
          <!-- List -->
          <div>
             <q-list>
-               <q-item v-for="(item, i) in 3" :key="i" tag="label" :class="{'custom-shadow': i==0}" class="bg-white round-10 q-mt-md">
+               <q-item v-for="(item, i) in localCartItems" :key="i" tag="label" :class="{'custom-shadow': i==0}" class="bg-white round-10 q-mt-md">
                   <q-item-section side top>
-                     <q-checkbox v-model="check1" />
+                     <q-checkbox v-model="selected" :val="item.id" />
                   </q-item-section>
 
                   <!-- Label -->
                   <q-item-section>
                      <q-item-label>
-                        <p class="text-body1">Headphone Joss</p>
+                        <p class="text-body1">{{ item.name }}</p>
                      </q-item-label>
                      <q-item-label caption>
-                        <p class="flex items-center" style="margin-top: 5px;">
+                        <!-- <p class="flex items-center" style="margin-top: 5px;">
                            <span class="bg-grey-14 inline-block rounded-borders" style="width:19px; height:19px;"></span>
                            <span class="q-ml-sm inline-block ">Grey</span>
-                        </p>
-                        <p class="text-h6 text-black q-mt-sm text-bold">$254</p>
+                        </p> -->
+                        <p class="text-h6 text-black q-mt-sm text-bold">{{ item.formatted_price }}</p>
                      </q-item-label>
                      <!-- Quantity adjust -->
                      <q-item-label>
                         <div class="flex justify-end">
                            <div style="margin-top: -25px;">
-                              <q-btn dense unelevated @click="--quantity" color="grey-3" class="round-10 text-grey-6" icon="remove" />
-                              <q-btn outline dense unelevated color="grey-3" class="round-10 q-px-md text-grey-10 q-mx-sm">{{ quantity }}</q-btn>
-                              <q-btn dense unelevated @click="++quantity" color="grey-3" class="round-10 text-grey-6" icon="add" />
+                              <q-btn dense unelevated @click="--item.quantity" color="grey-3" class="round-10 text-grey-6" icon="remove" />
+                              <q-btn outline dense unelevated color="grey-3" class="round-10 q-px-md text-grey-10 q-mx-sm">{{ item.quantity }}</q-btn>
+                              <q-btn dense unelevated @click="++item.quantity" color="grey-3" class="round-10 text-grey-6" icon="add" />
                            </div>
                         </div>
                      </q-item-label>
@@ -100,8 +100,39 @@ export default {
    },
    data() {
       return {
-         check1: false,
-         quantity: 1
+         localCartItems: [],
+         selected: [],
+         checkAll: false
+      }
+   },
+   created() {
+      this.initLocaldata()
+   },
+   computed: {
+      allIds() {
+         return this.localCartItems.map(c => c.id)
+      }
+   },
+   methods: {
+      initLocaldata() {
+         const localCart = JSON.parse(localStorage.getItem('localCart'))
+         if (localCart && localCart.length) {
+            this.localCartItems = localCart
+         }
+      },
+      onCheckAll() {
+         console.log('check all')
+         if (this.checkAll) this.selected = this.allIds
+         else this.selected = []
+      }
+   },
+   watch: {
+      localCartItems(val) {
+         localStorage.setItem('localCart', JSON.stringify(val))
+      },
+      selected(val) {
+         if (val.length === this.allIds.length) this.checkAll = true
+         else this.checkAll = false
       }
    }
 }
