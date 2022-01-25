2e271074ed7633ba6d0313d3e56c5cdbf8f736ef
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-01-25 13:24:25 +0600

    added fonts

--- src/pages/orders/details.vue
+++ src/pages/orders/details.vue
@@ -1,12 +1,38 @@
 <template>
-   <div>
-      details
-   </div>
+   <app-layout>
+      <div v-if="order">
+         <q-card>
+            <q-card-section>
+               <h6>Order Information</h6>
+            </q-card-section>
+         </q-card>
+      </div>
+   </app-layout>
 </template>
 
 <script>
+import AppLayout from 'layouts/AppLayout.vue'
 export default {
-
+   components: {
+      AppLayout
+   },
+   data() {
+      return {
+         order: null
+      }
+   },
+   async created() {
+      try {
+         this.loadingOrders = true
+         const res = await this.$api.get(`/account/orders/${this.$route.params.id}`)
+         this.order = res.data
+         console.log(res.data)
+      } catch (error) {
+         console.log(error)
+      } finally {
+         this.loadingOrders = false
+      }
+   }
 }
 </script>
 
