229488d8728fdfb4ffaf3ab76fff8524b028777e
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-02-01 12:40:18 +0600

    updated

--- src/pages/auth/Signup.vue
+++ src/pages/auth/Signup.vue
@@ -61,11 +61,11 @@
             <q-btn rounded no-caps :loading="savingState" :disable="savingState" size="lg" type="submit" label="Sign Up" color="primary" class="full-width q-mt-lg" />
 
             <p class="text-grey-6 text-body1 text-center q-my-md">Or Sign up with</p>
-            <q-btn rounded outline no-caps size="lg" color="grey" class="full-width">
+            <q-btn rounded outline no-caps href="https://www.galaxytelecom.fr/api/login/facbook" size="lg" color="grey" class="full-width">
                <facebook-svg />
                <span class="q-ml-md">Continue With FB</span>
             </q-btn>
-            <q-btn rounded outline no-caps size="lg" color="grey" class="full-width q-mt-md">
+            <q-btn rounded outline no-caps href="https://www.galaxytelecom.fr/api/login/google" size="lg" color="grey" class="full-width q-mt-md">
                <google-svg />
                <span class="q-ml-md">Continue With Google</span>
             </q-btn>
